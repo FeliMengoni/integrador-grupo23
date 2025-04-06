84de2f3d79457e33ec3f8aa0538d3a7eedfa9944
Joaquin Sciarretta <joaquinsciarretta@gmail.com>
2025-04-06 19:49:17 -0300

    guardo la lista de hogar en una variable

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+# Obtener la ruta del archivo dentro de "datasets"
+ruta_archivo = os.path.join(os.path.dirname(__file__), "datasets", "usu_hogar_T324.txt")
+
+# Leer el archivo y convertirlo en una lista
+with open(ruta_archivo, "r", encoding="utf-8") as file:
+    lista_datos = file.read().splitlines()  # Divide el contenido por líneas
+
+print(lista_datos)  # Ver la lista resultante
