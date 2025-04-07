7caf30a1df12a5096295f3dceb3649c4c5385a01
Joaquin Sciarretta <joaquinsciarretta@gmail.com>
2025-04-07 14:04:16 -0300

    creo una variable con los datos de el usu hogar 2024 t 3

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from src import cleandata
 
 
 ruta_archivo = os.path.join(os.path.dirname(__file__), "datasets", "usu_hogar_T324.txt")
+with open(ruta_archivo, "r", encoding="utf-8") as archivo:
+    data_base = [linea.strip().split(";") for linea in archivo]
+
+
 
 #lista_datos.dropna(inplace=True)  # Eliminar filas con valores nulos
-lista_datos = cleandata.cleaner(lista_datos)  # Limpiar los datos
-print(lista_datos)
+lista_datos = cleandata.cleaner(data_base)  # Limpiar los datos
+print(data_base[16000])
