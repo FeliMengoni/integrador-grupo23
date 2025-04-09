776cb838013d80944298d82c9b21728c83c47aff
Joaquin Sciarretta <joaquinsciarretta@gmail.com>
2025-04-08 21:40:22 -0300

    hago utils para Hogares, y defino algunas funciones

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ carpeta_individuos = os.path.join(os.path.dirname(__file__), "datasets", "indivi
 archivos_hogar = [archivo for archivo in os.listdir(carpeta_hogar) if archivo.endswith(".txt")]
 archivos_individuos = [archivo for archivo in os.listdir(carpeta_individuos) if archivo.endswith(".txt")]
 
-#ruta_archivo = os.path.join(os.path.dirname(__file__), "datasets", "usu_hogar_T324.txt")
+#ruta_archivo = os.path.join(os.path.dirname(__file__), "datasets","hogar", "usu_hogar_T424.txt")
 #with open(ruta_archivo, "r", encoding="utf-8") as archivo:
- ##   data_base = [linea.strip().split(";") for linea in archivo]
+#    data_base = [linea.strip().split(";") for linea in archivo]
 
 
 
 #lista_datos.dropna(inplace=True)  # Eliminar filas con valores nulos
 #data_base = cleandata.cleaner(data_base)  # Limpiar los datos
 #print(data_base)
-print("Archivo 2 de la carpeta hogar")
-print(archivos_hogar[0])
-print("Archivo 2 de la carpeta individuos")
-print(archivos_individuos[0])
+#print("Archivo 2 de la carpeta hogar")
+#print(archivos_hogar[0])
+#print("Archivo 2 de la carpeta individuos")
+#print(archivos_individuos[0])
