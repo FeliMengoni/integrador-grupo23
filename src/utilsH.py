a6d9df96caf831f7522646bf7ba63d41ee41e3ca
Paz Fernandez Oliver <pazfernandezoliver@gmail.com>
2025-04-12 15:34:38 -0300

    funcion UtilsH, pasada a diccionario

--- src/utilsH.py
+++ src/utilsH.py
@@ -18,9 +18,11 @@ def material_techumbre (diccionario_de_datos):
                 linea['MATERIAL_TECHUMBRE']='Material durable'
             case 5|6|7:
                 linea['MATERIAL_TECHUMBRE']= 'Material precario'
+            case 9:
+                linea['MATERIAL_TECHUMBRE']= ' No aplica'
 
-def densidad_hogar (diccionario_de_datos):
-    """Funcion que agrega culumna de densidad de hogar"""
+"""def densidad_hogar (lista_de_datos):
+    #Funcion que agrega culumna de densidad de hogar
     lista_de_datos[0].append('DENSIDAD_HOGAR')
     indice_de_IX_TOT = lista_de_datos[0].index('IX_TOT')
     indice_de_II1 = lista_de_datos[0].index('II1')
@@ -31,7 +33,18 @@ def densidad_hogar (diccionario_de_datos):
         elif personas_por_habitacion <= 2:
             linea.append('Medio')
         else:
-            linea.append('Alto')
+            linea.append('Alto')"""
+
+def densidad_hogar(diccionario_de_datos):
+    for linea in diccionario_de_datos.items():
+        personas_por_habitacion = int(linea["IX_TOT"]) / int(linea["II1"])
+        if personas_por_habitacion < 1:
+            linea["DENSIDAD_HOGAR"] = "Bajo"
+        elif personas_por_habitacion <= 2:
+            linea["DENSIDAD_HOGAR"] = "Medio"
+        else:
+            linea["DENSIDAD_HOGAR"] = "Alto"
+
 
 def condicion_de_habitabilidad (lista_de_datos):  #NO ESTA TERMINADA (hay q hablarlo)
     """Funcion que agrega culumna de condicion de habitabilidad"""
