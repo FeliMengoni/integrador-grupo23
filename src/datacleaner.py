ea92fe5448cecaa50e107a7ddf6fc07f5d51e0db
Joaquin Sciarretta <joaquinsciarretta@gmail.com>
2025-04-14 15:07:12 -0300

    cleaner hogares

--- src/datacleaner.py
+++ src/datacleaner.py
@@ -1,6 +1,8 @@
 from src import utils
+from src import utilsH
 
-def cleaner (lista_dic):
+def cleanerU (lista_dic):
+
     for dic in lista_dic:
         for dato in dic:
             if dic[dato].isnumeric():
@@ -15,3 +17,19 @@ def cleaner (lista_dic):
         utils.nivel_to_string(dic)
         utils.condicion_laboral_to_string(dic)
         utils.universitario(dic)
+
+def cleanerH (lista_dic):
+    for dic in lista_dic:
+        for dato in dic:
+            if dic[dato].isnumeric():
+                dic[dato] = int(dic[dato])
+            elif not dic[dato]:
+                dic[dato] = None
+            elif isinstance(dic[dato], str):
+                dic[dato] = dic[dato].strip()
+            else:
+                dic[dato] = None
+    #utilsH.condicion_de_habitabilidad(lista_dic)
+    utilsH.densidad_hogar(lista_dic)
+    utilsH.material_techumbre(lista_dic)
+    utilsH.tipo_hogar(lista_dic)
--- src/utilsH.py
+++ src/utilsH.py
@@ -1,12 +1,12 @@
 def tipo_hogar (diccionario_de_datos):
     """Funcion que agrega culumna de tipo de hogar"""
     for linea in diccionario_de_datos:
-        match int(linea("IX_TOT")):
+        match int(linea["IX_TOT"]):
             case 1:
                 linea['TIPO_HOGAR']= 'unipersonal'
             case 2|3|4:
                 linea['TIPO_HOGAR']='Nuclear'
-        if int(linea('IX_TOT')) >= 5:
+        if int(linea['IX_TOT']) >= 5:
             linea['TIPO_HOGAR']= 'Extensa'
 
 
@@ -19,25 +19,15 @@ def material_techumbre (diccionario_de_datos):
             case 5|6|7:
                 linea['MATERIAL_TECHUMBRE']= 'Material precario'
             case 9:
-                linea['MATERIAL_TECHUMBRE']= ' No aplica'
+                linea['MATERIAL_TECHUMBRE']= 'No aplica'
 
-"""def densidad_hogar (lista_de_datos):
-    #Funcion que agrega culumna de densidad de hogar
-    lista_de_datos[0].append('DENSIDAD_HOGAR')
-    indice_de_IX_TOT = lista_de_datos[0].index('IX_TOT')
-    indice_de_II1 = lista_de_datos[0].index('II1')
-    for linea in lista_de_datos[1:]:
-        personas_por_habitacion = int(linea[indice_de_IX_TOT])/int(linea[indice_de_II1])
-        if personas_por_habitacion <1:
-            linea.append('Bajo')
-        elif personas_por_habitacion <= 2:
-            linea.append('Medio')
-        else:
-            linea.append('Alto')"""
 
 def densidad_hogar(diccionario_de_datos):
-    for linea in diccionario_de_datos.items():
-        personas_por_habitacion = int(linea["IX_TOT"]) / int(linea["II1"])
+    for linea in diccionario_de_datos:
+        if int(linea["II1"])!=0:
+            personas_por_habitacion = (int(linea["IX_TOT"])/int(linea["II1"]))
+        else:
+            personas_por_habitacion = 5
         if personas_por_habitacion < 1:
             linea["DENSIDAD_HOGAR"] = "Bajo"
         elif personas_por_habitacion <= 2:
