7331e29f07145e87f6edb5a08ffb0b53b6c13dc0
FeliMengoni <69765257+FeliMengoni@users.noreply.github.com>
2025-04-14 16:02:38 -0300

    personas extranjeras que hayan cursado universitario o mayor

--- src/utilsB.py
+++ src/utilsB.py
@@ -23,14 +23,20 @@ def porcentaje_de_leer(diccionario): #Falta terminar
             conteos["Porcentaje No Capaces"]=(conteos['no_capaces']/total)*100
     return resultados
 
-def no_nacidas (lista): # Recibe la lista de diccionarios
+def no_nacidas (lista): # Recibe la lista de diccionarios / CH15: donde nacio
     """Porcentaje de personas no nacidas en ARG que hayan cursado nivel universitario o mayor"""
     anio = int(input('Inserte el año: '))
     trim = int(input('Inserte el trimestre: '))
+    cant_personas = 0
+    aprobados = 0
     for dic in lista:
         if (anio < dic['ANO4']) or (dic['ANO4'] == anio and trim < dic['TRIMESTRE']):
             continue
         elif anio == dic['ANO4'] and trim == dic['TRIMESTRE']:
-            
+            if dic['CH15'] == 4 or dic['CH15'] == 5:
+                cant_personas = dic['PONDERA'] + cant_personas
+                if (dic['CH12'] == 7) or dic['CH12'] == 8:
+                    aprobados = aprobados + dic['PONDERA']
         else:
             break
+    print(round(aprobados/cant_personas*100, 2))
