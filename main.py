38a2034937b663c03d8805a48552581d3c23c55b
Joaquin Sciarretta <joaquinsciarretta@gmail.com>
2025-04-13 21:56:04 -0300

    pruebo

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
-import pathlib
+from pathlib import Path
 import csv
 from src import utils
 from src import datacleaner
 
 #unimos las bases de datos de hogar e individuo
 
-#ruta_individuos = Path('datasets') / 'individual' / 'usu_individual_T423.txt'
+ruta_individuos = Path('datasets') / 'usu_individual_T423.txt'
 
-#with open(ruta_individuos, 'r', encoding='utf-8') as archivo_individuos:
-   #datosU = list(csv.DictReader(archivo_individuos, delimiter=';'))
+with open(ruta_individuos, 'r', encoding='utf-8') as archivo_individuos:
+   datosU = list(csv.DictReader(archivo_individuos, delimiter=';'))
 
-print(datosU[0])
 for line in datosU[:5]:
    datacleaner.cleaner(line)
    utils.genero_to_string(line)
